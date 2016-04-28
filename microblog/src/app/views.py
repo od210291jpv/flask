cf61d13624d3954acf6f550ff962e15e9cc0d091
user <od210291jpv@gmail.com>
2016-04-28 19:01:10 +0300

    Login form and login view partially implemented

--- microblog/src/app/views.py
+++ microblog/src/app/views.py
@@ -1,6 +1,8 @@
 __author__ = 'user'
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
+from flask.ext.wtf import Form
 
 @app.route('/')
 @app.route('/index')
@@ -13,3 +15,14 @@ def index():
              {'author': {'nickname': 'Whois'},
               'body': 'Knigaknigakniga!!'}]
     return render_template("index.html", title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_sumbit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+                           title = 'Sign in',
+                           form = form)
